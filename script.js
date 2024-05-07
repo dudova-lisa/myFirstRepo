ce0eb2b36f24c9c55e18db50e52e9fc405a0f5db
dudova-lisa <lizadudova91@gmail.com>
2024-05-07 20:19:44 +0200

    fix the first task

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const title = 1;
-const screen = 2;
-const screenPrice = 2;
-const rollback = 4;
-const FullPrice = 5;
-const adaptive = 6;
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 
 alert("Hello world!");
 console.log("My name is...");
