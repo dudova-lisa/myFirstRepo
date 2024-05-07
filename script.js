b4bed20be5c87955faf0c2d8a783881bd05f84c2
dudova-lisa <lizadudova91@gmail.com>
2024-05-07 18:46:59 +0200

    выполнено второе задание

---
+++ script.js
@@ -0,0 +1,21 @@
+const title = "Мой курс JavaScript";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 8;
+const rollback = 67;
+const fullPrice = 10000;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(screenPrice); //"Стоимость верстки экранов" "рублей/долларов/гривен/юани"
+
+console.log(fullPrice); //"Стоимость разработки сайта" "рублей/долларов/гривен/юани"
+
+console.log(screens.toLowerCase());
+console.log(screens.split("ы"));
+
+console.log(fullPrice * (rollback / 100)); //"Процент отката посреднику за работу"
